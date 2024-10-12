447738ecc19ec217aaf96f22340dbdcfd798f996
vitor <vi.souza.almeida@protonmail.com>
2024-10-12 15:36:25 -0300

    feat(chat): add optional host argument (#4)

--- main.go
+++ main.go
@@ -1,11 +1,27 @@
 package main
 
 import (
+	"flag"
 	"html/template"
 	"log"
 	"net/http"
 	"sync"
 )
+
+func main() {
+	var addr = flag.String("addr", ":8080", "The addr of the application.")
+	flag.Parse()
+	r := newRoom()
+	http.Handle("/", &templateHandler{filename: "templates/chat.html"})
+	http.Handle("/room", r)
+	go r.run()
+
+	log.Printf("Starting web server on %s%s", "http://localhost", *addr)
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
 
 type templateHandler struct {
 	once     sync.Once
@@ -17,17 +33,5 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(t.filename))
 	})
-	t.templ.Execute(w, nil)
-}
-
-func main() {
-	r := newRoom()
-	http.Handle("/", &templateHandler{filename: "templates/chat.html"})
-	http.Handle("/room", r)
-
-	go r.run()
-
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal("ListenAndServe: ", err)
-	}
+	t.templ.Execute(w, r)
 }
