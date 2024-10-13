9825df3d78315eae099d314e1a20ca8432306f70
vitor <vi.souza.almeida@protonmail.com>
2024-10-13 11:57:27 -0300

    feat(trace): add trace package

---
+++ trace/tracer.go
@@ -0,0 +1,23 @@
+package trace
+
+import (
+	"fmt"
+	"io"
+)
+
+type Tracer interface {
+	Trace(...interface{})
+}
+
+func New(w io.Writer) Tracer {
+	return &tracer{out: w}
+}
+
+type tracer struct {
+	out io.Writer
+}
+
+func (t *tracer) Trace(a ...interface{}) {
+	fmt.Fprint(t.out, a...)
+	fmt.Fprintln(t.out)
+}
